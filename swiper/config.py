4f1d0e8b8caa8d5c5be3572b4e4be11567887903
root <root@ubuntu16.04>
2019-10-09 16:56:46 +0800

    merage

---
+++ swiper/config.py
@@ -0,0 +1,11 @@
+'''第三方平台的配置'''
+
+YZX_SMS_API = 'https://open.ucpaas.com/ol/sms/sendsms'
+YZX_SMS_PARAMS = {
+    "sid": 'a88108ca93a370050637e49ec1c905de',
+    "token": '730223d4cd49a0169ba5141e6d208cc6',
+    "appid": '44b8828cb3aa408e9b8b74059d6f2f1b',
+    "templateid": "487820",
+    "param": None,
+    "mobile": None,
+}
---
+++ swiper/urls.py
@@ -0,0 +1,27 @@
+"""swiper URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+
+from user import api as user_api
+
+
+urlpatterns = [
+    url(r'api/user/submit/phone', user_api.submit_phone),
+#    url(r'api/user/submit/vcode', user_api.submit_vcode),
+#    url(r'api/user/get/profile', user_api.get_profile),
+#    url(r'api/user/set/profile', user_api.set_profile),
+#    url(r'api/user/upload/avatar', user_api.upload_avatar),
+]
